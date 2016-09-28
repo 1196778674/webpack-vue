cbaeec4a712ddec7411563b23185aecd4bcbebd3
liufei <liufei@chupinxiu.com>
2016-09-28 18:51:31 +0800

    res

---
+++ src/views/index.vue
@@ -0,0 +1,19 @@
+<template>
+{{index}}
+</template>
+
+<script>
+export default {
+
+  name: 'index',
+
+  data () {
+    return {
+    	index : 'index!!!!'
+    };
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
