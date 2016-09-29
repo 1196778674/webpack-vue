57d19468ea4fc2474a6a2116154466be5c5979af
liufei <liufei@chupinxiu.com>
2016-09-29 10:15:48 +0800

    methods

--- src/views/usercenter/usercenter.vue
+++ src/views/usercenter/usercenter.vue
@@ -1,15 +1,23 @@
 <template>
-{{usercenter}}
+	{{usercenter}}
+	<button type="button" @click="test()">button</button>
 </template>
 
 <script>
 export default {
 
   name: 'usercenter',
-
+  methods: {
+  	aaa(a) {
+  		console.log(a);
+  	},
+    test () {
+    	this.aaa(1);
+    }
+  },
   data () {
     return {
-    	usercenter: 'usercenter!!!!'
+    	usercenter: "usercenter!!!!!!!"
     };
   }
 };
