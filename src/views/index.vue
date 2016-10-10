05ec4fe42a5f844d2165275337dbe76dd4f21a0c
liufei <liufei@chupinxiu.com>
2016-10-10 16:43:18 +0800

    index

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,9 +1,29 @@
 <template>
-{{index}}
 
-<date-picker></date-picker>
-<span>至</span>
-<date-picker></date-picker>
+<div class="index-container">
+  <div class="cpx-row index-row">
+    <h3>{{title[0]}}</h3>
+    <div class="index-datepacker">
+      <span>时间:</span>
+      <date-picker></date-picker>
+      <span>至</span>
+      <date-picker></date-picker>
+    </div>
+    <div class="index-basedata">
+      <ul v-for="base in basedata">
+        <li class="number">{{base.number}}</li>
+        <li class="basedata-type">{{base.type}}</li>
+      </ul>
+    </div>
+  </div>
+  <div class="cpx-row index-row">
+    <h3>{{title[1]}}</h3>
+  </div>
+  <div class="cpx-row index-row">
+    <h3>{{title[2]}}</h3>
+  </div>
+</div>
+
 
 <button type="button" class="btn btn-default" @click="getTime">button</button>
 
@@ -23,7 +43,9 @@ export default {
   },
   data () {
     return {
-    	index : 'index!!!!'
+    	index : 'index!!!!',
+      title: ["基础数据","今日统计","活跃用户"],
+      basedata: [{"number":"2343","type":"注册门店数"},{"number":"4234","type":"注册用户数"},{"number":"3462","type":"付费门店数"},{"number":"3643","type":"库房系统使用数"},{"number":"2343","type":"签约门店"}]
     };
   },
   components: {
