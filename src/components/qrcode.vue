57ecd9bb81053292e27ce4469d1b789ea6f7326b
liufei <liufei@chupinxiu.com>
2016-10-10 14:19:58 +0800

    qrcode

---
+++ src/components/qrcode.vue
@@ -0,0 +1,27 @@
+<template>
+<div id="qrcode"></div>
+</template>
+
+<script>
+var qrcode = require('../../node_modules/qrcode/src/index.js')
+export default {
+
+  name: 'qrcode',
+  methods: {
+    qrcode () {
+    	var qrnode = new qrcode({
+	        text: 'http://www.baidu.com/'
+	    });
+	    $(document).ready(function() {
+	        $('#qrcode').html(qrnode);
+	    });
+    }
+  },
+  data () {
+  	this.qrcode()
+    return {
+    	qrcode: 'qrcode'
+    };
+  }
+};
+</script>
--- src/views/index.vue
+++ src/views/index.vue
@@ -4,12 +4,14 @@
 <date-picker></date-picker>
 <span>至</span>
 <date-picker></date-picker>
-<div id="qrcodeDefault"></div>
+
 <button type="button" class="btn btn-default" @click="getTime">button</button>
+
+<take-qrcode></take-qrcode>
 </template>
 
 <script>
-var qrcode = require('../../node_modules/qrcode/src/index.js')
+
 export default {
 
   name: 'index',
@@ -17,25 +19,17 @@ export default {
   methods: {
     getTime () {
       console.log($('.nowdate').first().text(),$('.nowdate').last().text());
-    },
-    show () {
-      var qrnode = new qrcode({
-        text: 'http://www.baidu.com/'
-      });
-      $(document).ready(function() {
-        $('#qrcodeDefault').html(qrnode);
-      });
     }
   },
   data () {
-    this.show()
     return {
     	index : 'index!!!!'
     };
   },
   components: {
   	replace: false,
-  	datePicker: require('../components/datepicker.vue')
+  	datePicker: require('../components/datepicker.vue'),
+    takeQrcode: require('../components/qrcode.vue')
   }
 
 };
